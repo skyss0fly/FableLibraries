7284b3e1e6fe6f4ef0f88e92f36eb10aa510bb87
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-08-25 19:59:32 +1000

    Basic Math Library for FableLang
    Still in development

---
+++ Libraries/FabMath/MathLibrary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FabMath
+{
+     public static class MathLibrary
+	 {
+	    public static double Sin(double x) => Math.Sin(x);
+		public static double Cos(double x) => Math.Cos(x);
+		public static double Sqrt(double x) => Math.Sqrt(x);
+		public static double Pow(double x, double y) => Math.Pow(x, y);
+		public static double Abs(double x) => Math.Abs(x);
+	}
+}
+	
